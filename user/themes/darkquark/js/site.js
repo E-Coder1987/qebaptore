036bed58022638cbbe7a2902d1760b01c77dcde7
E-Coder1987 <e-coder@e-coder.eu>
2026-02-08 22:19:50 +0100

    Mobile CSS Fixes

--- user/themes/darkquark/js/site.js
+++ user/themes/darkquark/js/site.js
@@ -2,11 +2,11 @@ var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
 
 function scrollHeader() {
     // Has scrolled class on header
-    var zvalue = $(document).scrollTop();
-    if ( zvalue > 75 )
-        $("#header").addClass("scrolled");
-    else
-        $("#header").removeClass("scrolled");
+    // var zvalue = $(document).scrollTop();
+    // if ( zvalue > 75 )
+    //     $("#header").addClass("scrolled");
+    // else
+    //     $("#header").removeClass("scrolled");
 }
 
 function parallaxBackground() {
@@ -47,13 +47,60 @@ jQuery(document).ready(function($){
     });
 
     // Responsive Menu
+// Responsive Menu
     $('#toggle').click(function () {
         $(this).toggleClass('active');
         $('#overlay').toggleClass('open');
         $('body').toggleClass('mobile-nav-open');
     });
+    
+    // Mobile Menu Links - ERST scrollen, DANN schließen
+    $('.overlay-menu a').on('click', function(e) {
+        var href = $(this).attr('href');
+        
+        if (href && href.startsWith('#')) {
+            e.preventDefault();
+            
+            var target = $(href);
+            
+            if (target.length) {
+                // ERST scrollen
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 180
+                }, 500, function() {
+                    // DANN Menu schließen (nach dem Scrollen)
+                    $('#toggle').removeClass('active');
+                    $('#overlay').removeClass('open');
+                    $('body').removeClass('mobile-nav-open');
+                });
+            }
+        }
+    });
 
     // Tree Menu
     $(".tree").treemenu({delay:300});
-
+// Smooth Scroll für alle Anchor-Links mit Header-Offset
+    $('a[href^="#"]').on('click', function(e) {
+        var target = $(this.attr('href'));
+        
+        // Nur für anchor links (#...) und wenn Target existiert
+        if (target.length && this.hash) {
+            e.preventDefault();
+            
+            // Overlay schließen falls offen
+            $('#toggle').removeClass('active');
+            $('#overlay').removeClass('open');
+            $('body').removeClass('mobile-nav-open');
+            
+            // Scroll mit Offset - Mobile 100px, Desktop 80px
+            var header_offset = $(window).width() <= 840 ? 100 : 80;
+            
+            window.scroll({ 
+                top: target.offset().top - header_offset, 
+                left: 0, 
+                behavior: 'smooth' 
+            });
+        }
+    });
+    
 });
